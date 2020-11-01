475817b4f930e2b67e0fa55b173b06fa1efffc97
ŃaSrO <bouaouina.nasreddine@gmail.com>
2020-11-01 22:58:31 +0100

    initial Author Routes

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const dotenv = require("dotenv");
 const expressLayouts = require("express-ejs-layouts");
 const indexRouter = require("./routes/index");
+const authorRouter = require("./routes/author");
 
 // Load config
 dotenv.config({ path: "./config/config.env" });
@@ -16,7 +17,11 @@ app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// routes
 app.use("/", indexRouter);
+app.use("/authors", authorRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT);
