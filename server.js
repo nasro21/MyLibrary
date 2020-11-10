f02e7692685d159a90df8d13729f281a8d8f482a
ŃaSrO <bouaouina.nasreddine@gmail.com>
2020-11-10 23:32:10 +0100

    Books Create/Index/New

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const expressLayouts = require("express-ejs-layouts");
+
 const indexRouter = require("./routes/index");
 const authorRouter = require("./routes/author");
+const bookRouter = require("./routes/books");
 
 // Load config
 dotenv.config({ path: "./config/config.env" });
@@ -19,9 +21,11 @@ app.use(expressLayouts);
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 // routes
 app.use("/", indexRouter);
 app.use("/authors", authorRouter);
+app.use("/books", bookRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT);
